a136c1dcce8067684351cee1bdd72592532d97ce
Antoine <bouhahah@gmail.com>
2016-05-03 19:31:29 +0200

    Routing angular et page d'inscription

--- public/app.js
+++ public/app.js
@@ -1,7 +1,52 @@
-var app = angular.module('LO07-publi', []);
+var app = angular.module('LO07-publi', ['ui.router']);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider){
+
+    $stateProvider.state('home',{
+      url: '/home',
+      templateUrl: 'home',
+      controller: 'Home'
+    });
+
+    $stateProvider.state('register', {
+      url: '/register',
+      templateUrl: 'register',
+      controller: 'Register'
+    });
+
+    $urlRouterProvider.otherwise('home');
+}]);
 
 app.controller('Home', [
 '$scope',
-function($scope){
+'$state',
+function($scope, $state){
+  
   $scope.text = 'Une application web de gestion du publication!';
+
+  $scope.register = function(){
+    $state.go("register");
+  };
+}]);
+
+
+app.controller('Register', [
+    '$scope',
+    '$http',
+    '$state',
+    function($scope, $http, $state){
+
+
+      $scope.equipes = ["CREIDD", "ERA", "GAMMA3", "LASMIS", "LM2S", "LNIO", "LOSI", "Tech-CICO"];
+
+      $scope.annuler = function(){
+        $state.go("home");
+      };
+
+      $scope.register = function(){
+        //TODO : envoyer les données du formulaire au serveur
+      }
 }]);
