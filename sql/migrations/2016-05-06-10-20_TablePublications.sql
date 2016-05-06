ccded3d1b4a2e9cd7b0d70040255652439d159a6
Victor Levasseur <victorlevasseur52@gmail.com>
2016-05-06 10:34:46 +0200

    Ajout d'une migration pour la table publications

---
+++ sql/migrations/2016-05-06-10-20_TablePublications.sql
@@ -0,0 +1,16 @@
+# Migration de la base de données
+#
+# Date et heure : 6 mai 2016 à 10:20
+#
+# Rôle : Créer la table Publications
+
+create table Publications(
+    id int not null primary key auto_increment,
+    title varchar(512) not null,
+    description varchar(65536),
+    status enum('submitted', 'published', 'reviewing') not null,
+    publication_title varchar(512),
+    publication_year int,
+    conference_location varchar(512),
+    file_id int not null
+);
