c9947be71420e5f8ed8468decee3c3e03ea2bbd9
ElDawgerino <bouhahah@gmail.com>
2016-05-20 13:50:46 +0200

    Routes dans un fichier séparée

--- public/app.js
+++ public/app.js
@@ -1,42 +1,4 @@
-var app = angular.module('LO07-publi', ['ui.router', 'auth-module', 'naif.base64', 'publi-module']);
-
-app.config([
-  '$stateProvider',
-  '$urlRouterProvider',
-  function($stateProvider, $urlRouterProvider, $rootScope){
-
-    $stateProvider.state('home',{
-      url: '/home',
-      templateUrl: 'public/templates/home.html',
-      controller: 'Home'
-    });
-
-    $stateProvider.state('register', {
-      url: '/register',
-      templateUrl: 'public/templates/register.html',
-      controller: 'Register'
-    });
-
-    $stateProvider.state('login', {
-      url: '/login',
-      templateUrl: 'public/templates/login.html',
-      controller: 'Login'
-    });
-
-    $stateProvider.state('publish', {
-      url: '/publish',
-      templateUrl: 'public/templates/publish.html',
-      controller: 'Publish'
-    });
-
-    $stateProvider.state('publi', {
-      url: '/publi/{id}',
-      templateUrl: 'public/templates/publi.html',
-      controller: 'Publi',
-    });
-
-    $urlRouterProvider.otherwise('home');
-}]);
+var app = angular.module('LO07-publi', ['ui.router', 'auth-module', 'naif.base64', 'publi-module', 'routes-module']);
 
 app.controller('Home', [
     '$scope',
---
+++ public/routes.js
@@ -0,0 +1,39 @@
+var app = new angular.module('routes-module', ['ui.router']);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider, $rootScope){
+
+    $stateProvider.state('home',{
+      url: '/home',
+      templateUrl: 'public/templates/home.html',
+      controller: 'Home'
+    });
+
+    $stateProvider.state('register', {
+      url: '/register',
+      templateUrl: 'public/templates/register.html',
+      controller: 'Register'
+    });
+
+    $stateProvider.state('login', {
+      url: '/login',
+      templateUrl: 'public/templates/login.html',
+      controller: 'Login'
+    });
+
+    $stateProvider.state('publish', {
+      url: '/publish',
+      templateUrl: 'public/templates/publish.html',
+      controller: 'Publish'
+    });
+
+    $stateProvider.state('publi', {
+      url: '/publi/{id}',
+      templateUrl: 'public/templates/publi.html',
+      controller: 'Publi',
+    });
+
+    $urlRouterProvider.otherwise('home');
+}]);
